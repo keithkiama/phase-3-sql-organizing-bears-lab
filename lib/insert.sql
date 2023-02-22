41510e38827520f170670cf584a313cab88b1630
Keith Kiama Maina <117682417+keithkiama@users.noreply.github.com>
2023-02-22 11:36:06 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'crimson', 'pleasant', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 1, 'M', 'brown', 'depressed', 'no');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'white', 'apathetic', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'white', 'warm', 'no');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 8, 'F', 'black', 'sad', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'black', 'quick to react', 'no');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 1, 'F', 'white', 'hot tempered', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 11, 'M', 'white', 'slow-moving', 'yes');
